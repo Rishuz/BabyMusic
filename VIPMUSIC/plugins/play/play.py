60e54b5da810605efc7a574bef4be2326453183e
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-28 14:48:15 +0530

    Update play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -1,12 +1,4 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 import asyncio
 import random
 import string
@@ -728,7 +720,7 @@ async def slider_queries(client, CallbackQuery, _):
         )
 
 
-__MODULE__ = "Plᴀʏ"
+__MODULE__ = "• ᴘʟᴀʏ •"
 __HELP__ = """
 <b>★ ᴘʟᴀʏ , ᴠᴘʟᴀʏ , ᴄᴘʟᴀʏ</b> - Aᴠᴀɪʟᴀʙʟᴇ Cᴏᴍᴍᴀɴᴅs
 <b>★ ᴘʟᴀʏғᴏʀᴄᴇ , ᴠᴘʟᴀʏғᴏʀᴄᴇ , ᴄᴘʟᴀʏғᴏʀᴄᴇ</b> - FᴏʀᴄᴇPʟᴀʏ Cᴏᴍᴍᴀɴᴅs
