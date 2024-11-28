93345156683b0e3120c15dc4261d89793e1ffd93
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-28 14:36:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
+
 import os
 import re
 from os import getenv
@@ -45,7 +38,7 @@ DURATION_LIMIT_MIN = int(
 
 EXTRA_PLUGINS = getenv(
     "EXTRA_PLUGINS",
-    "True",
+    "False",
 )
 
 # Fill True if you want to load extra plugins
