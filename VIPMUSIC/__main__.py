868d51896e860ff96812fa5801808e41c5806574
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-29 09:06:41 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -12,7 +12,7 @@ import config
 from config import BANNED_USERS
 from VIPMUSIC import HELPABLE, LOGGER, app, userbot
 from VIPMUSIC.core.call import VIP
-from VIPMUSIC.plugins import ALL_MODULES
+
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
 
 
